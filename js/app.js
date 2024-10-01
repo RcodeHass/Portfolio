fc128a721954b6a3b2a76634d453b8114953f4d4
Ridjali Hassani <hassani9@live.fr>
2024-10-01 19:41:59 +0200

    first version 1.0.0

--- js/app.js
+++ js/app.js
@@ -8,3 +8,75 @@ const openMenu = () => {
         document.querySelector("header .material-symbols-outlined").innerHTML = "menu"
     }
 }
+
+
+// Les boutton switch
+
+// const carousel = document.querySelector (".carousel");
+// const arrowBtns = document.querySelectorAll (".wrapper .fleche");
+// const firtCardWidth = carousel.querySelector (".card").offsetWidth; 
+
+// let isDragging = false, startX, startScrollLeft;
+
+
+// arrowBtns.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         carousel.scrollLeft += btn.id === "left" ? - firtCardWidth : firtCardWidth;
+//     })
+// })
+
+// const dragStart = (e) => {
+//     isDragging = true;
+//     carousel.classList.add("dragging");
+//     startX = e.pageX;
+//     startScrollLeft = carousel.scrollLeft;
+// }
+
+// const dragging = (e) => {
+//     if(!isDragging) return;
+//     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+// }
+// const dragStop = () => {
+//     isDragging = false;
+//     carousel.classList.remove ("dradding");
+// }
+// carousel.addEventListener("mousedown", dragStart);
+// carousel.addEventListener("mousemove", dragging);
+// carousel.addEventListener("mouseup", dragStop);
+
+const carousels = document.querySelectorAll(".carousel");
+
+carousels.forEach((carousel, index) => {
+    const arrowBtns = document.querySelectorAll(`#glr_${index + 1} .fleche`); // Flèches spécifiques à chaque galerie
+    const firstCardWidth = carousel.querySelector(".card").offsetWidth; 
+
+    let isDragging = false, startX, startScrollLeft;
+
+    arrowBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+        });
+    });
+
+    const dragStart = (e) => {
+        isDragging = true;
+        carousel.classList.add("dragging");
+        startX = e.pageX;
+        startScrollLeft = carousel.scrollLeft;
+    };
+
+    const dragging = (e) => {
+        if (!isDragging) return;
+        carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+    };
+
+    const dragStop = () => {
+        isDragging = false;
+        carousel.classList.remove("dragging");
+    };
+
+    // Ajout des événements pour chaque carousel
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("mousemove", dragging);
+    carousel.addEventListener("mouseup", dragStop);
+});
