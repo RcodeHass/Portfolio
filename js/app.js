be32a3080b7c732e948ed85760d894cebf978dc8
Ridjali Hassani <hassani9@live.fr>
2024-11-03 14:04:36 +0100

    first version 1.1.5

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-const openMenu = () => {
-    const menu = document.querySelector (".header-menu");
-    menu.classList.toggle("activ");
-    if(menu.classList.contains("activ")){
-        document.querySelector("header .material-symbols-outlined").innerHTML = "close"
-    }
-    else{ 
-        document.querySelector("header .material-symbols-outlined").innerHTML = "menu"
-    }
-}
+// const openMenu = () => {
+//     const menu = document.querySelector (".header-menu");
+//     menu.classList.toggle("activ");
+//     if(menu.classList.contains("activ")){
+//         document.querySelector("header .material-symbols-outlined").innerHTML = "close"
+//     }
+//     else{ 
+//         document.querySelector("header .material-symbols-outlined").innerHTML = "menu"
+//     }
+// }
 
 
 // Les boutton switch
@@ -79,4 +79,43 @@ carousels.forEach((carousel, index) => {
     carousel.addEventListener("mousedown", dragStart);
     carousel.addEventListener("mousemove", dragging);
     carousel.addEventListener("mouseup", dragStop);
+});
+
+
+// Centre la visualisation 
+document.querySelectorAll('.scroll-link').forEach(link => {
+    link.addEventListener('click', function (event) {
+        // Empêche le comportement par défaut de l'ancre
+        event.preventDefault();
+        
+        // Obtenir la cible à partir de l'attribut href du lien
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        // Faire défiler vers l'élément avec alignement centré
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center'
+            });
+        }
+    });
+});
+
+const openMenu = () => {
+    const menu = document.querySelector(".header-menu");
+    menu.classList.toggle("activ");
+    if (menu.classList.contains("activ")) {
+        document.querySelector(".header-menu-mobile .material-symbols-outlined").innerHTML = "close";
+    } else {
+        document.querySelector(".header-menu-mobile .material-symbols-outlined").innerHTML = "menu";
+    }
+};
+
+// Masquer le menu après un clic sur un lien
+document.querySelectorAll('.scroll-link').forEach(link => {
+    link.addEventListener('click', function () {
+        document.querySelector('.header-menu').classList.remove('activ');
+        document.querySelector(".header-menu-mobile .material-symbols-outlined").innerHTML = "menu"; // Réinitialiser l'icône
+    });
 });
