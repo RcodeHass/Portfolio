606e4851b1058923f3d32f0a958eefd839539c32
Ridjali Hassani <hassani9@live.fr>
2026-02-03 23:19:18 +0100

    Porfolio version 2.5.0

--- js/main.js
+++ js/main.js
@@ -1,24 +1,87 @@
-document.querySelectorAll('.gallery-thumb').forEach(link => {
-link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const images = JSON.parse(this.getAttribute('data-images'));
-    const title = this.getAttribute('data-title') || 'Galerie';
-    const carouselInner = document.getElementById('galleryCarouselInner');
-    carouselInner.innerHTML = '';
+const competencesmasterstage2 = [
+    "../assets/images/stage_m2/stg_m2_1.png",
+    "../assets/images/stage_m2/stg_m2_2.png",
+    "../assets/images/stage_m2/stg_m2_3.png",
+    "../assets/images/stage_m2/stg_m2_4.png",
+    "../assets/images/stage_m2/stg_m2_5.png",
+    "../assets/images/stage_m2/stg_m2_6.png"
+];
+
+
+
+
+
+const competencesImages = [
+    "../assets/images/stage_m1/stage_1.png",
+    "../assets/images/stage_m1/stage_2.png",
+    "../assets/images/stage_m1/stage_3.png",
+    "../assets/images/stage_m1/stage_4.png",
+    "../assets/images/stage_m1/stage_5.png",
+    "../assets/images/stage_m1/stage_6.png"
+];
+
+const competencesFme = [
+    "../assets/images/fme/fme_1.png",
+    "../assets/images/fme/fme_2.png",
+    "../assets/images/fme/fme_3.png"
+];
+
+const competencesAgriculture = [
+    "../assets/images/chambre-agriculture/agri_1.png",
+    "../assets/images/chambre-agriculture/agri_2.png",
+    "../assets/images/chambre-agriculture/agri_3.png",
+    "../assets/images/chambre-agriculture/agri_4.png",
+    "../assets/images/chambre-agriculture/agri_5.png"
+];
+
+const competencesAnalyse = [
+    "../assets/images/analyse-spatial/as_1.png",
+    "../assets/images/analyse-spatial/as_2.png",
+    "../assets/images/analyse-spatial/as_3.png",
+    "../assets/images/analyse-spatial/as_4.png"
+];
+
+const exp_etude_impact = [
+    "../assets/images/etude-impact/impact_1.png",
+    "../assets/images/etude-impact/impact_2.JPG",
+    "../assets/images/etude-impact/impact_3.png",
+    "../assets/images/etude-impact/impact_4.png",
+    "../assets/images/etude-impact/impact_5.png"
+];
+
+function buildCarousel(images, containerId) {
+    const container = document.getElementById(containerId);
+    container.innerHTML = "";
 
     images.forEach((src, index) => {
-    const img = document.createElement('img');
-    img.src = src;
-    img.className = 'd-block w-100';
-    img.alt = title;
+        const item = document.createElement("div");
+        item.className = "carousel-item" + (index === 0 ? " active" : "");
 
-    const item = document.createElement('div');
-    item.className = 'carousel-item' + (index === 0 ? ' active' : '');
-    item.appendChild(img);
-    carouselInner.appendChild(item);
+        item.innerHTML = `
+            <img src="${src}" class="d-block w-100 rounded" loading="lazy">
+        `;
+
+        container.appendChild(item);
     });
+}
 
-    document.getElementById('galleryTitle').textContent = title;
-    new bootstrap.Modal(document.getElementById('galleryModal')).show();
-});
+document.addEventListener("DOMContentLoaded", () => {
+
+    buildCarousel(competencesmasterstage2, "carouselStageM2Inner");
+    // buildCarousel(competencesmasterstage2, "carouselStageM2ModalInner");
+
+    buildCarousel(competencesFme, "carouselFmeInner");
+    // buildCarousel(competencesFme, "carouselFmeModalInner");
+    
+    buildCarousel(competencesAgriculture, "carouselAgricultureInner");
+    // buildCarousel(competencesAgriculture, "carouselAgricultureModalInner");
+
+    buildCarousel(competencesAnalyse, "carouselAnalyseInner");
+    // buildCarousel(competencesAnalyse, "carouselAnalyseModalInner");
+
+    buildCarousel(competencesImages, "carouselCompetencesInner");
+    // buildCarousel(competencesImages, "carouselCompetencesModalInner");
+
+    buildCarousel(exp_etude_impact, "carouselEtudeImpactInner");
+    // buildCarousel(exp_etude_impact, "carouselEtudeImpactModalInner");
 });
